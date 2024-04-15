08371105bd2bb0df12fda1984d76a78ee9e2c1fa
NamHyoeun11 <nhe0104@gmail.com>
2024-04-15 12:03:39 +0900

    0415 - SmartHome, Music

---
+++ Chap2_Array_Class/Chap02_Class_Example/Music.h
@@ -0,0 +1,74 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Music class
+class Music {
+private:
+    string title;
+    string artist;
+    string album;
+    int year;
+public:
+    //생성자 (Constructor)
+    Music(string title, string artist, string album, int year) {
+        this->title = title;
+        this->artist = artist;
+        this->album = album;
+        this->year = year;
+    }
+
+    //소멸자(Destructor)
+    ~Music() {}
+
+    // Getters
+    string getTitle() { return title; }
+    string getArtist() { return artist; }
+    string getAlbum() { return album; }
+    int getYear() { return year; }
+};
+
+// MusicStreamingService class
+class MusicStreamingService {
+private:
+    string serviceName;
+    vector<Music> musicList;    //Music형태의 vector 생성 
+
+public:
+    MusicStreamingService(string serviceName) {
+        this->serviceName = serviceName;
+    }
+
+    // Adds a music to the service
+    void addMusic(string title, string artist, string album, int year) {
+        Music newMusic(title, artist, album, year);
+        musicList.push_back(newMusic);  //맨 뒤에 newMusic 객체 넣기
+        cout << title << " by " << artist << " added to " << serviceName << endl;
+    }
+
+    // Searches for music by title
+    //포인터 
+    Music* searchByTitle(string title) {
+        for (int i = 0; i < musicList.size(); i++) {
+            if (musicList[i].getTitle() == title) {
+                return &musicList[i];   //musicList[i]의 주소값 반환
+            }
+        }
+        return NULL;
+    }
+
+    // Searches for music by artist
+    vector<Music*> searchByArtist(string artist) {
+        vector<Music*> result;
+        for (int i = 0; i < musicList.size(); i++) {
+            if (musicList[i].getArtist() == artist) {
+                result.push_back(&musicList[i]);
+            }
+        }
+        return result;
+    }
+
+};
--- Chap2_Array_Class/Chap02_Class_Example/main.cpp
+++ Chap2_Array_Class/Chap02_Class_Example/main.cpp
@@ -1,8 +1,13 @@
 #include <cstdio>
 //#include "Car.h"
 //#include "SportsCar.h"
-/*
+#include "Rectangle.h"
+#include "SmartHome.h"
+#include "Music.h"
+using namespace std;
+
 void main() {
+	/*
 	//Car 클래스 사용
 	Car myCar(50, "K3", 4);
 	Car yourCar(100, "K5", 3);
@@ -15,9 +20,7 @@ void main() {
 	SportsCar scar;
 	scar.speedUp();
 
-}*/
-#include "Rectangle.h"
-using namespace std;
+}
 
 void main() {
 	Rectangle r(10, 20);
@@ -27,5 +30,45 @@ void main() {
 	std::cout << "Area:" << r.getArea() << std::endl;
 	cout << "Perimeter" << perimeter << endl;
 	cout << "Is square?" << boolalpha << r.isSquare() << endl;
+}
+
+//SmartHome클래스
+
+
+	SmartHome myHome("남효은", 23, 40, true);
+	myHome.printStatus();
+	*/
+	
+	MusicStreamingService myService("Spotify");
+	myService.addMusic("VIBE", "TaeYang", "Album", 2023);
+	myService.addMusic("Ditto", "NewJeans", "Album", 2023);
+	myService.addMusic("Attention", "NewJeans", "Album", 2022);
+
+	string music_title;
+	cout << "Enterthe Music Title: ";
+	cin >> music_title;
+
+	Music* result = myService.searchByTitle(music_title);
+	if (result != NULL) {
+		cout << "Found: " << result->getTitle() << " by " << result->getArtist() << endl;
+	}
+	else {
+		cout << "Not found" << endl;
+	}
+
+	string artist_name;
+	cout << "Enter the Artist Name: ";
+	cin >> artist_name;
+
+	vector<Music*> artistResult = myService.searchByArtist(artist_name);
+	if (artistResult.size() > 0) {
+		cout << "Found " << artistResult.size() << " songs by " << artist_name << " : " << endl;
+			for (int i = 0; i < artistResult.size(); i++) {
+				cout << artistResult[i]->getTitle() << endl;
+		}
+	}
+	else {
+		cout << "Not found" << endl;
+	}
 
 }
